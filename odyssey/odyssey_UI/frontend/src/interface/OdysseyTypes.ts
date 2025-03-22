26254b37e93385b7481f2516c0bfc2aec12f99ad
Runitupronnie <runitupronnie@gmail.com>
2025-03-21 19:08:59 -0700

    Re-add odyssey_UI as a regular directory

---
+++ odyssey/odyssey_UI/frontend/src/interface/OdysseyTypes.ts
@@ -0,0 +1,47 @@
+// OdysseyTypes.ts
+export interface FeeValue {
+    amount: string;
+    category: string;
+    destination: string;
+}
+
+export interface FeeStage {
+    key: string;
+    value: FeeValue[];
+}
+
+export interface OdysseyResource {
+    collection: {
+        inner: string;
+    };
+    collection_size: string;
+    cover: string;
+    description: string;
+    fees: {
+        data: FeeStage[];
+    };
+    minted: string;
+    paused: boolean;
+}
+
+export interface Stage {
+    key: string;
+    value: {
+      start_time: number;
+      end_time: number;
+      stage_mint_limit: string;
+      stage_minted: string;
+    };
+}
+  
+export interface FeesData {
+    key: string;
+    value: {
+      amount: string; // Note: amount is still considered as string
+    }[];
+}
+  
+export interface Fees {
+    key: string;
+    amount: string; // Note: amount is still considered as string
+}
